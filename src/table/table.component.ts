570b94331381c7261bfd584d01e1c3c5c86c1996
unknown <regionstats@yahoo.com>
2018-03-18 21:22:11 -0500

    tried to improve the table styles. still not centered

--- src/table/table.component.ts
+++ src/table/table.component.ts
@@ -24,12 +24,13 @@ export class TableComponent {
     public tableRowMap: { [region: string]: TableRow } = {};
     public stats: Stat[];
     public rowWidth: string;
-    public scrollLeft: number;
-    public primaryLineLeft: string;
-    public secondaryLineLeft: string;
+    public scrollLeft: number = 0;
+    public primaryLineLeft: number = -120;
+    public secondaryLineLeft: number = -120;
 
     private verticalScrollFunctionRef: (e) => {};
-    private horizontalScrollFunctionRef: (e) => {};
+    private headerScrollFunctionRefs: Array<(e) => {}> = [];
+    private dataScrollFunctionRef: (e) => {};
     private fixedHeader: boolean = false;
     private scrollContainer: HTMLElement;
     private selectedIndexes: number[] = [0, 1];
@@ -44,30 +45,27 @@ export class TableComponent {
 
         this.dataService.getSelectedIndexes().subscribe((indexes) => {
             this.selectedIndexes = indexes;
-            this.primaryLineLeft = (10 + 6 * indexes[0]) + "rem";
-            if (this.indexCount >= 2) {
-                this.secondaryLineLeft = (10 + 6 * indexes[1]) + "rem";
-            } else {
-                this.secondaryLineLeft = "-6rem";
-            }
+            this.updateLines();
         });
         this.dataService.getStats().subscribe(stats => {
             this.stats = stats;
-            this.rowWidth = (stats.length * 6) + "rem";
+            this.rowWidth = (stats.length * 120) + "px";
             this.updateTableRows();
             setTimeout(() => {
-                this.horizontalScrollFunctionRef = this.horizontallyScrolled.bind(this);
-                this.tableData.nativeElement.addEventListener("scroll", this.horizontalScrollFunctionRef);
+                let headers = document.getElementsByClassName("table-header");
+                for (let i = 0; i < headers.length; i++){
+                    let headerScrollFunctionRef = this.headerScrolled.bind(this);
+                    headers[i].addEventListener("scroll", headerScrollFunctionRef);
+                    this.headerScrollFunctionRefs.push(headerScrollFunctionRef);
+                }
+                this.dataScrollFunctionRef = this.dataScrolled.bind(this);
+                this.tableData.nativeElement.addEventListener("scroll", this.dataScrollFunctionRef);
             });
         });
     }
 
     ngOnChanges(changes: SimpleChanges) {
-        if (this.indexCount >= 2) {
-            this.secondaryLineLeft = (10 + 6 * this.selectedIndexes[1]) + "rem";
-        } else {
-            this.secondaryLineLeft = "-6rem";
-        }
+        this.updateLines();
     }
 
     headerClicked(event: MouseEvent, index: number) {
@@ -84,23 +82,44 @@ export class TableComponent {
         }
         this.dataService.setSelectedIndexes(this.selectedIndexes);
     }
+
+    private updateLines() {
+        this.primaryLineLeft = (160 + 120 * this.selectedIndexes[0]) - this.scrollLeft;
+        if (this.indexCount >= 2) {
+            this.secondaryLineLeft = (160 + 120 * this.selectedIndexes[1]) - this.scrollLeft;
+        } else {
+            this.secondaryLineLeft = -120;
+        }
+    }
+
     verticallyScrolled(e: Event) {
         var header = <HTMLDivElement>this.componentContainer.nativeElement;
         if (!this.fixedHeader && this.scrollContainer.scrollTop > header.offsetTop) {
-            //header.setAttribute("style", "position: fixed");
             this.fixedHeader = true;
         } else if (this.fixedHeader && this.scrollContainer.scrollTop < header.offsetTop){
-            //header.setAttribute("style", "")
             this.fixedHeader = false;
         }
     }
 
-    horizontallyScrolled(e: Event) {
-        this.scrollLeft = this.tableData.nativeElement.scrollLeft;
-        let headerRows = document.getElementsByClassName("table-header-row");
-        for (var i = 0; i < headerRows.length; i++){
-            headerRows[i].setAttribute("style", `margin-left: ${-this.scrollLeft}px; width: ${this.rowWidth};`);
+    headerScrolled(e: Event) {
+        this.scrollLeft = (<any>e.target).scrollLeft;
+        let headers = document.getElementsByClassName("table-header");
+        for (var i = 0; i < headers.length; i++){
+            //headerRows[i].setAttribute("style", `margin-left: ${-this.scrollLeft}px; width: ${this.rowWidth};`);
+            headers[i].scrollLeft = this.scrollLeft;
         }
+        this.tableData.nativeElement.scrollLeft = this.scrollLeft;
+        this.updateLines();
+    }
+
+    dataScrolled(e: Event) {
+        this.scrollLeft = this.tableData.nativeElement.scrollLeft;
+        let headers = document.getElementsByClassName("table-header");
+        for (var i = 0; i < headers.length; i++){
+            //headerRows[i].setAttribute("style", `margin-left: ${-this.scrollLeft}px; width: ${this.rowWidth};`);
+            headers[i].scrollLeft = this.scrollLeft;
+        }
+        this.updateLines();
     }
 
     updateTableRows() {
